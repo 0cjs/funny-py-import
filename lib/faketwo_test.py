cd3e17a523c6b854b3aa578238e0c7d1999e1a4f
Curt J. Sampson <cjs@cynic.net>
2019-04-18 16:46:31 +0900

    faketwo_test: Also check sys.modules versions of what was loaded

--- lib/faketwo_test.py
+++ lib/faketwo_test.py
@@ -7,10 +7,12 @@ def test_faketwo_local_import():
 
     #   `two` module is not loaded, nor bound at the top level of this module.
     assert ABSENT is sys.modules.get('two', ABSENT)
+    assert ABSENT is sys.modules.get('two.faketwo', ABSENT)
     assert ABSENT is globals().get('two', ABSENT)
 
-    #   After load, it's bound locally but not globally
     import two.faketwo
+
+    #   After load, it's bound locally but not globally
     assert ABSENT is globals().get('two', ABSENT)
     assert two
     assert two.faketwo
@@ -30,6 +32,15 @@ def test_faketwo_local_import():
     assert 'attr set in two/faketwo.py' == two.faketwo.faketwo_version
     #   And it definitely does not have anything from replacement.
     assert ABSENT is getattr(two.faketwo, 'two_replacement', ABSENT)
+
+    #   Modules were entered into sys.modules.
+    sysmod_two = sys.modules.get('two', ABSENT)
+    assert ABSENT is not sysmod_two
+    sysmod_two_faketwo = sys.modules.get('two.faketwo', ABSENT)
+    assert ABSENT is not sysmod_two_faketwo
+    #   But, as above, contain binding to faketwo rather than the replacement
+    assert doc_original == sysmod_two_faketwo.__doc__
+    assert ABSENT is getattr(sysmod_two_faketwo, 'two_replacement', ABSENT)
 
     #   In other words, our binding of `fakeone` created with the
     #   `import fakeone` statement above appears to have been bound to
